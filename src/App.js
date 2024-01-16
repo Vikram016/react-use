23e7984f7a48fea298f830d4fc979645c616c916
Vikram016 <jbvikram@outlook.com>
2024-01-16 20:26:24 +0530

    useSate

---
+++ src/App.js
@@ -0,0 +1,32 @@
+import './App.css';
+import { useState } from "react";
+import AnimalShow from "./AnimalShow";
+
+
+
+function getRandomAnimal(){
+    const animals = ['bird', 'cat', 'dog', 'gator', 'horse'];
+
+    return animals[Math.floor(Math.random() * animals.length)];
+
+}
+
+function App(){
+    const [animals,setAnimals] = useState([]);
+
+    const handleClick = () =>{
+        setAnimals([...animals, getRandomAnimal()]);
+    };
+ const renderdAnimals = animals.map((animal, index) => {
+return <AnimalShow type={animal} key={index} />
+ });
+    
+    return (
+        <div className="app">
+            <button onClick={handleClick}>Add Animal</button>
+            <div className="animal-list">{renderdAnimals}</div>
+        </div>
+    );
+}
+
+export default App;
